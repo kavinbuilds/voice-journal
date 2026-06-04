2ed179fc8a0d78d500f2049856349579b92eaf9f
kavin <nskavin0606@gmail.com>
2026-06-04 19:58:13 +0530

    Enhance diary app with audio recording and TTS
    Added functionality to record audio and convert it to diary notes. Implemented search by date and read diary note aloud using text-to-speech.

--- journal.py
+++ journal.py
@@ -3,8 +3,16 @@ import streamlit as st
 from datetime import datetime
 import os
 import whisper
+from gtts import gTTS
+import tempfile
 #load the whisper model
 model=whisper.load_model("base")
+if "audio_key" not in st.session_state:
+    st.session_state.audio_key=0
+if "edit_area" not in st.session_state:
+    st.session_state["edit_area"]=""
+if "diary_note" not in st.session_state:
+    st.session_state["diary_note"]=""
 st.set_page_config(page_title="voice to diary🗣️➡️📔", page_icon=":speech_balloon:", layout="centered")
 st.markdown("<h6> Voice to Diary🗣️➡️📔</h6>",unsafe_allow_html=True)
 
@@ -23,42 +31,56 @@ st.markdown(
         unsafe_allow_html=True
     )
 diary_note=""
-option=st.sidebar.selectbox("select an option",["search diary","add diary"],key="sidebar_option")
-if option=="add diary":
-    st.text("Upload your audio file here and convert to diary note")
-    audio_file=st.file_uploader("upload your audio file here",type=['mp3','wav', 'm4a'])
+tab1,tab2=st.tabs(["**Add Diary**","**Search Diary**"])
+with tab1:
+    st.markdown("Upload your audio file here and convert to diary note")
+    audio_file=st.audio_input("record audio",key=f"audio_input_{st.session_state.audio_key}")
     if audio_file is not None:
         with open("audio.mp3","wb") as f:
             f.write(audio_file.read())
         st.audio(audio_file)
+        record=st.button("clear audio",key=f"clear_audio_{st.session_state.audio_key}",type="secondary",use_container_width=True,width="stretch")
+        if record:
+            st.session_state.audio_key+=1
+            if os.path.exists("audio.mp3"):
+                os.remove("audio.mp3")
+                st.rerun()
 
-    if st.button("convert to diary", key="convert",type="primary",use_container_width=True,width="stretch"):
-        result=model.transcribe("audio.mp3")
-        st.session_state["edit_area"]=result["text"]
-        edit_area=st.text_area("Your diary note", value=st.session_state["edit_area"], height=200)
-        #save the diary note to a text file
-        if st.button("save diary note",key='save',type='primary'):
-            os.makedirs("diary_notes", exist_ok=True)
-            time=datetime.now().strftime("%d-%m-%y")
-            filename=f"diary_notes/{time}.txt"
+        if st.button("convert to diary", key="convert",type="primary",use_container_width=True,width="stretch") and audio_file is not None:
+            with st.spinner("Converting voice to text... 🎙️"):
+                result = model.transcribe("audio.mp3")
+            st.success("Conversion completed ✅")
+            st.session_state["edit_area"]=result["text"]
             edit_area=st.text_area("Your diary note", value=st.session_state["edit_area"], height=200)
-            with open(filename,'w',encoding='utf-8') as f:
-                st.write("check the diary note...")
-                f.write(edit_area)
-            st.success(f"Diary note saved as {filename}")
+        #save the diary note to a text file
+            if st.button("save diary note",key='save',type='primary',width="stretch",use_container_width=True):
+                os.makedirs("diary_notes", exist_ok=True)
+                time=datetime.now().strftime("%d-%m-%y")
+                filename=f"diary_notes/{time}.txt"
+                edit_area=st.text_area("Your diary note", value=st.session_state["edit_area"], height=200)
+                with open(filename,'w',encoding='utf-8') as f:
+                    st.write("check the diary note...")
+                    f.write(edit_area)
+                st.success(f"Diary note saved as {filename}")
 
 
-if option=="search diary":
-    with st.sidebar:
-        st.subheader("search by date")
-        search_date=st.date_input("select date",key="search_date")
-        if st.button("search",key="search",type="primary",use_container_width=True,width="stretch`"):
-            format_date=search_date.strftime("%d-%m-%y")
-            filename=f"diary_notes/{format_date}.txt"
-            if os.path.exists(filename):
-                with open(filename,'r',encoding='utf-8') as f:
-                    diary_note=f.read()
-    if diary_note:
-        st.text_area("diary note on " + search_date.strftime("%d-%m-%y"),value=diary_note,height=200)
-    else:
-        st.warning("no diary note found for the selected date")
+with tab2:
+    st.subheader("search by date")
+    search_date=st.date_input("select date",key="search_date")
+    if st.button("search",key="search",type="primary",use_container_width=True,width="stretch"):
+        format_date=search_date.strftime("%d-%m-%y")
+        filename=f"diary_notes/{format_date}.txt"
+        if os.path.exists(filename):
+            with open(filename,'r',encoding='utf-8') as f:
+                st.session_state["diary_note"]=f.read()
+        else:
+            st.warning("no diary note found for the selected date")
+    if st.session_state["diary_note"]:
+        text_area=st.text_area("diary note on " + search_date.strftime("%d-%m-%y"),value=st.session_state["diary_note"],height=200)
+        if st.button("🔊 Read Diary") and st.session_state["diary_note"].strip()!="":
+            tts = gTTS(text=st.session_state["diary_note"], lang='en')
+            with tempfile.NamedTemporaryFile(delete=False, suffix=".mp3") as fp:
+                tts.save(fp.name)
+                audio_file = open(fp.name, "rb")
+                audio_bytes = audio_file.read()
+            st.audio(audio_bytes, format="audio/mp3")
