8e3eb7ef8c6ddc364c63648cc1a2b5940c6ab276
kavin <nskavin0606@gmail.com>
2026-08-12 23:28:38 +0530

    Refactor audio input and model loading
    Refactor audio handling and model loading in the diary app. Change audio file format from mp3 to wav and implement caching for the Whisper model.

--- journal.py
+++ journal.py
@@ -7,7 +7,10 @@ import tempfile
 
 st.set_page_config(page_title="voice to diary🗣️➡️📔", page_icon=":speech_balloon:", layout="centered")
 #load the whisper model
-model=whisper.load_model("base")
+@st.cache_resource
+def load_model():
+    return whisper.load_model("base")
+model=load_model()
 if "audio_key" not in st.session_state:
     st.session_state.audio_key=0
 if "edit_area" not in st.session_state:
@@ -36,37 +39,40 @@ with tab1:
     st.markdown("Upload your audio file here and convert to diary note")
     audio_file=st.audio_input("record audio",key=f"audio_input_{st.session_state.audio_key}")
     if audio_file is not None:
-        with open("audio.mp3","wb") as f:
+        with open("audio.wav","wb") as f:
             f.write(audio_file.read())
         st.audio(audio_file)
         record=st.button("clear audio",key=f"clear_audio_{st.session_state.audio_key}",type="secondary",width="stretch")
         if record:
             st.session_state.audio_key+=1
-            if os.path.exists("audio.mp3"):
-                os.remove("audio.mp3")
+            if os.path.exists("audio.wav"):
+                os.remove("audio.wav")
                 st.rerun()
 
         if st.button("convert to diary", key="convert",type="primary",width="stretch") and audio_file is not None:
             with st.spinner("Converting voice to text... 🎙️"):
-                result = model.transcribe("audio.mp3")
+                result = model.transcribe("audio.wav")
             st.success("Conversion completed ✅")
             st.session_state["edit_area"]=result["text"]
-            edit_area=st.text_area("Your diary note", value=st.session_state["edit_area"], height=200)
+            if st.session_state["edit_area"]:
+                st.write("check the diary note...")
+                edit_area=st.text_area("Your diary note", value=st.session_state["edit_area"], height=200)
+                st.session_state["edit_area"]=edit_area
         #save the diary note to a text file
-            if st.button("save diary note",key='save',type='primary',width="stretch"):
-                os.makedirs("diary_notes", exist_ok=True)
-                time=datetime.now().strftime("%d-%m-%y")
-                filename=f"diary_notes/{time}.txt"
-                with open(filename,'w',encoding='utf-8') as f:
-                    st.write("check the diary note...")
-                    f.write(edit_area)
-                st.success(f"Diary note saved as {filename}")
+        if st.button("save diary note",key='save',type='primary',width="stretch"):
+            os.makedirs("diary_notes", exist_ok=True)
+            time=datetime.now().strftime("%d-%m-%y")
+            filename=f"diary_notes/{time}.txt"
+            with open(filename,'w',encoding='utf-8') as f:
+                f.write(st.session_state["edit_area"])
+            st.success(f"Diary note saved as {filename}")
 
 
 with tab2:
     st.subheader("search by date")
     search_date=st.date_input("select date",key="search_date")
     if st.button("search",key="search",type="primary",width="stretch"):
+        st.session_state["diary_note"]=""
         format_date=search_date.strftime("%d-%m-%y")
         filename=f"diary_notes/{format_date}.txt"
         if os.path.exists(filename):
